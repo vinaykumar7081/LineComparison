a69c6aa153c4bd3db13beaa015528fea76ef66a2
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-07 15:05:16 +0530

    [VINAY] Add: Comparision of the tow Lines in the Cartesian Plane

--- LengthOfCartesionLine/Program.cs
+++ LengthOfCartesionLine/Program.cs
@@ -7,6 +7,18 @@ namespace LengthOfCartesionLine
         {
             LengthOfLine lineComparison = new LengthOfLine(2, 5, 6, 8);
             lineComparison.calculateLength();
+            LengthOfLine line1 = new LengthOfLine(2, 5, 6, 8);
+            line1.calculateLength();
+            LengthOfLine line2 = new LengthOfLine(2, 5, 6, 8);
+            line2.calculateLength();
+            if (line1.Equals(line2))
+            {
+                Console.WriteLine("Both Lines are equals ");
+            }
+            else
+            {
+                Console.WriteLine("Both Lines are not equals ");
+            }
         }
     }
 }
