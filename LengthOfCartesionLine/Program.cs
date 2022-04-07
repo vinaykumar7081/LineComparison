81e066f4fe1c4a167e03361d25a98fdb00e7c812
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-07 14:50:34 +0530

    [VINAY] Add: lenght of the cartesian product

---
+++ LengthOfCartesionLine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LengthOfCartesionLine
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            LengthOfLine lineComparison = new LengthOfLine(2, 5, 6, 8);
+            lineComparison.calculateLength();
+        }
+    }
+}
